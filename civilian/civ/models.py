fdaaf9314b7220c79b49126036023347d562d4ae
ryaanwells <ryaanwells@googlemail.com>
2013-02-12 20:44:28 +0000

    started DB's

--- civilian/civ/models.py
+++ civilian/civ/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Faction(models.Model):
+    name = models.CharField(max_length=60,unique=True)
+    members = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return self.name
+
+class Users(models.Model):
+    name = models.CharField(max_length=35,unique=True)
+    fact = models.ForeignKey(Faction)
+    money = models.IntegerField()
+    city = models.OneToOneField(City)
+
+    def __unicode__(self):
+        return self.name
