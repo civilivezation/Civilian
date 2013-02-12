bdc5c4f73a72a20cc8aba978aa4a6013462ca8c1
ryaanwells <ryaanwells@googlemail.com>
2013-02-12 19:11:12 +0000

    started project

---
+++ civilian/civ/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from civ import views
+
+urlpatterns = patterns('',
+                       url(r'^$',views.index,name='index'),
+                       url(r'^home/',views.home,name='home'),
+)
