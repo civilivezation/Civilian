5d7acd28e092b9be6a04c88ccc0b26aa19c0d2c5
ryaanwells <ryaanwells@googlemail.com>
2013-03-05 18:30:15 +0000

    Added more update

--- civilian/update.py
+++ civilian/update.py
@@ -1,14 +1,27 @@
-from civ.models import Building, City
+from civ.models import Users, Building, City
 
-farmProfit = Building.objects.get(buildtype="Farm").profit
-labProfit = Building.objects.get(buildtype="Farm").profit
-barrackProfit = Building.objects.get(buildtype="Farm").profit
-studioProfit = Building.objects.get(buildtype="Farm").profit
-
-
-for city in City.objects.all():
-    city.money = city.money + city.farms*farmProfit
-    city.money = city.money + city.labs*labProfit
-    city.money = city.money + city.barracks*barrackProfit
-    city.money = city.money + city.studios*studioProfit
+farm = Building.objects.get(buildtype="Farm")
+farmProfit = farm.profit
+farmContrib = [farm.pfood,farm.part,farm.pmilitary,farm.pscience]
+lab = Building.objects.get(buildtype="Farm")
+labProfit = lab.profit
+labContrib = [lab.pfood,lab.part,lab.pmilitary,lab.pscience]
+barrack = Building.objects.get(buildtype="Farm")
+barrackProfit = barrack.profit
+barrackContrib = [barrack.pfood,barrack.part,barrack.pmilitary,barrack.pscience]
+studio = Building.objects.get(buildtype="Farm")
+studioProfit = studio.profit
+studioContrib = [studio.pfood,studio.part,studio.pmilitary,studio.pscience]
+for user in Users.objects.all():
+    city = user.city
+    fact = user.fact
+    profit = 0
+    factcontrib = 0
+    profit = city.farms*farmProfit
+    profit = city.labs*labProfit
+    profit = city.barracks*barrackProfit
+    profit = city.studios*studioProfit
+    city.money = city.money+profit-city.totalpopulation
     print city.money
+    city.save()
+    factcontrib = c
