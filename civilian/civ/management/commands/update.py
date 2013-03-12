80b59fe36a05674197b6b6d00b82db83f4deb62b
ryaanwells <ryaanwells@googlemail.com>
2013-03-12 13:06:14 +0000

    Fixed update script

--- civilian/civ/management/commands/update.py
+++ civilian/civ/management/commands/update.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
         farm = Building.objects.get(buildtype="Farm")
         farmProfit = farm.profit
         farmContrib = [farm.pfood,farm.part,farm.pmilitary,farm.pscience]
-        lab = Building.objects.get(buildtype="Farm")
+        lab = Building.objects.get(buildtype="Lab")
         labProfit = lab.profit
         labContrib = [lab.pfood,lab.part,lab.pmilitary,lab.pscience]
-        barrack = Building.objects.get(buildtype="Farm")
+        barrack = Building.objects.get(buildtype="Barracks")
         barrackProfit = barrack.profit
         barrackContrib = [barrack.pfood,barrack.part,barrack.pmilitary,barrack.pscience]
-        studio = Building.objects.get(buildtype="Farm")
+        studio = Building.objects.get(buildtype="Studio")
         studioProfit = studio.profit
         studioContrib = [studio.pfood,studio.part,studio.pmilitary,studio.pscience]
         for user in Users.objects.all():
@@ -31,16 +31,11 @@ class Command(BaseCommand):
             profit = profit + city.barracks*barrackProfit
             profit = profit + city.studios*studioProfit
             city.money = (character.money*profit)+city.money-city.totalpopulation
-            self.stdout.write("Money: {0}\n".format(city.money))
             city.save()
             fcontrib = city.farms*(farmContrib[0] + labContrib[0] + barrackContrib[0] + studioContrib[0])*character.food
             acontrib = city.studios*(farmContrib[1] + labContrib[1] + barrackContrib[1] + studioContrib[1])*character.arts
             lcontrib = city.labs*(farmContrib[3] + labContrib[3] + barrackContrib[3] + studioContrib[3])*character.science
             bcontrib = city.barracks*(farmContrib[2] + labContrib[2] + barrackContrib[2] + studioContrib[2])*character.military
-            self.stdout.write("Food Contribution: {0}\n".format(fcontrib))
-            self.stdout.write("Art Contribution:  {0}\n".format(acontrib))
-            self.stdout.write("Lab Contribution:  {0}\n".format(lcontrib))
-            self.stdout.write("Army Contribution: {0}\n".format(bcontrib))
             fact.score = fact.score + fcontrib + 2*acontrib + 4*bcontrib + 8*lcontrib
             fact.food = fact.food + fcontrib
             fact.art = fact.art + acontrib
